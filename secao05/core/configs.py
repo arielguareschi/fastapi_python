db20014e0afd4f10d56ff7510bf3c58c3411026d
arielguareschi <arielguareschi@gmail.com>
2022-12-26 09:28:38 -0300

    ajuste pass

--- secao05/core/configs.py
+++ secao05/core/configs.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:13031526@localhost:5432/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://postgres:masterkey@localhost:5432/faculdade'
     class Config:
         case_sensitive = True
 
--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -1,3 +1,4 @@
+from typing import List
 from pydantic import BaseSettings, AnyHttpUrl
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,6 +11,18 @@ class Settings(BaseSettings):
     DB_URL: str = 'postgresql+asyncpg://postgres:masterkey@localhost:5432/faculdade'
     #DB_URL: str = 'firebird+fdb://SYSDBA:masterkey@10.11.12.247:3050/aero32db/siga/aerosml.gdb'
     DBBaseModel = declarative_base()
+    
+    JWT_SECRET: str = 'ldUfdO-I2Wi0_MJXF8EPE3LW4yB0a308JoeEpVNRnO8'  
+    '''
+    import secrets 
+    
+    token: str = secrets.token_urlsafe(32)
+    '''
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTS: int = 60 * 24 * 7
+    
+    
+    
 
     class Config:
         case_sensitive = True
