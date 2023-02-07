c97422c6a740e9c54e1a0b4247d07667420c2b23
arielguareschi <arielguareschi@gmail.com>
2023-02-07 09:51:31 -0300

    Alteracao qualquer

--- secao05/main.py
+++ secao05/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from core.configs import settings
 from api.v1.api import api_router
 
-app: FastAPI = FastAPI(title='Curso API - FastAPI SQLModel')
+app: FastAPI = FastAPI(title='Curso API - FastAPI SQLModel 2')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
